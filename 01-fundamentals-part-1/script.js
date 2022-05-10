ddb75de077bad70d30165c396e11973f2b16a4b6
rucp <rucp33@gmail.com>
2022-05-10 20:47:42 -0300

    Update

---
+++ 01-fundamentals-part-1/script.js
@@ -0,0 +1,26 @@
+let js = 'amazing';
+console.log(40+8+23-10);
+
+console.log('Ruan');
+console.log(24);
+
+let firstName = 'Maria';
+console.log(firstName);
+console.log(firstName);
+console.log(firstName);
+
+let jonas_matilda = 'JM';
+let $function = 26;
+
+let person = 'jonas';
+let PI = 3.1415;
+
+// write clear variables
+let myFirstJob = 'Coder';
+let myCurrentJob = 'Teacher';
+
+// dont do this
+let job1 = 'programmer';
+let job2 = 'teacher';
+
+console.log(myFirstJob);
