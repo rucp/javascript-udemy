53e01e06abb73da3df6d2638097bac6ef098f3e8
rucp <rucp33@gmail.com>
2022-05-10 21:56:51 -0300

    Update

--- 01-fundamentals-part-1/script.js
+++ 01-fundamentals-part-1/script.js
@@ -1,3 +1,4 @@
+/*
 let js = 'amazing';
 console.log(40+8+23-10);
 
@@ -7,7 +8,7 @@ console.log(24);
 let firstName = 'Maria';
 console.log(firstName);
 console.log(firstName);
-console.log(firstName);
+// console.log(firstName);
 
 let jonas_matilda = 'JM';
 let $function = 26;
@@ -24,3 +25,44 @@ let job1 = 'programmer';
 let job2 = 'teacher';
 
 console.log(myFirstJob);
+*/
+
+// lecture: values and variables
+
+let country = 'Brazil';
+let continent = 'South America';
+let population = 212;
+
+console.log(country, continent, population); 
+
+/*
+let javascriptIsFun = true;
+console.log(javascriptIsFun);
+
+// console.log(typeof true);
+console.log(typeof javascriptIsFun);
+//console.log(typeof 23);
+// console.log(typeof 'Jonas');
+
+javascriptIsFun = 'YES!';
+console.log(typeof javascriptIsFun);
+
+let year;
+console.log(year, typeof year);
+
+year = 2022;
+console.log(year, typeof year);
+
+// doenst make any sense
+console.log(typeof null); 
+*/
+
+// lecture: data types
+
+let isIsland = false;
+let language;
+
+console.log(isIsland, typeof isIsland);
+console.log(population, typeof population);
+console.log(country, typeof country);
+console.log(language, typeof language);
