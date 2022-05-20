cff2d1c430f1528328beeac2657180f2e7ea7c02
rucp <rucp33@gmail.com>
2022-05-20 19:16:05 -0300

    update

--- 02-fundamentals-part-2/challenges/script.js
+++ 02-fundamentals-part-2/challenges/script.js
@@ -34,7 +34,7 @@ const total = [bills[0] + tips[0],bills[1] + tips[1], bills[2] + tips[2]];
 console.log(total)
 */
 
-
+/*
 /// CHALLENGE #3
 
 const mark = {
@@ -68,3 +68,29 @@ if (mark.BMI > john.BMI) {
 } else {
     console.log('They have the same BMI')
 }
+*/
+
+const bills = [22, 295, 176, 440, 37, 105, 10, 1100, 86, 52];
+const tips = [];
+const totals = [];
+
+const calcTip = bill => bill >= 50 && bill <= 300 ? bill * 0.15 : bill * 0.2;
+
+for (let i = 0; i < bills.length; i++) {
+    tips.push(calcTip(bills[i]));
+    totals.push(tips[i] + bills[i]);
+}
+
+console.log(tips, bills, totals);
+
+
+function calcAverage(arr) {
+    let sum = 0;
+    for (let i = 0; i < arr.length; i++) {
+        sum += arr[i];
+    }
+    return sum / arr.length;
+}
+
+// console.log(calcAverage([2, 3, 4, 5]));
+console.log(calcAverage(tips));
---
+++ 03-Developer-Skills/script.js
@@ -0,0 +1,10 @@
+'use strict';
+
+const x = '23';
+if (x === 23) console.log(23);
+
+const calcAge = birthYear => 2022 - birthYear;
+
+console.log(1991);
+console.log('teste');
+console.log('half of a sec');
