48d51558205b3794ac0b38cb3e3d3d8ad584876a
rucp <rucp33@gmail.com>
2022-09-01 22:02:19 -0300

    closure

--- 10-Functions/script.js
+++ 10-Functions/script.js
@@ -227,6 +227,7 @@ console.log(addVAT2(100));
 
 */
 
+/*
 // 137
 
 const secureBooking = function () {
@@ -253,3 +254,68 @@ booker(); // 3 passengers
 // So, therefore the Booker function will get access to this variable environment which contains the 'passengerCount' variable. And this is how the function will be able to read and manipulate the 'passengerCounte' variable.
 
 // And so, it's this connection that we call closure.
+
+*/
+
+// /* 138 */
+
+// //example1
+// let f;
+
+// const g = function () {
+//   const a = 23;
+//   f = function () {
+//     console.log(a * 2);
+//   };
+// };
+
+// const h = function () {
+//   const b = 555;
+//   f = function () {
+//     console.log(b * 2);
+//   };
+// };
+
+// g();
+// f();
+// console.dir(f);
+
+// // re-assigning f function
+// h();
+// f();
+// console.dir(f);
+
+// //example 2
+// const boardPassengers = function (n, wait) {
+//   const perGroup = n / 3;
+
+//   setTimeout(function () {
+//     console.log(`We are now boarding all ${n} passengers`);
+//     console.log(`There was 3 groups, each with ${perGroup} passengers`);
+//   }, wait * 1000);
+
+//   console.log(`Will start boarding in ${wait} seconds`);
+// };
+
+// boardPassengers(180, 3);
+
+// Coding Challenge #2
+
+// This is more of a thinking challenge than a coding challenge 🤓
+
+// Your tasks:
+
+// 1. Take the IIFE below and at the end of the function, attach an event listener that changes the color of the selected h1 element ('header') to blue, each time the body element is clicked. Do not select the h1 element again
+
+// 2. And now explain to yourself (or someone around you) why this worked! Take all the time you need. Think about when exactly the callback function is executed, and what that means for the variables involved in this example.
+
+(function () {
+  console.log('funcao executada, mas nao clicada');
+  const header = document.querySelector('h1');
+  header.style.color = 'red';
+
+  document.body.addEventListener('click', function () {
+    header.style.color = 'blue';
+    console.log('funcao clicada');
+  });
+})();
